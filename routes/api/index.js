f0a07ab6c49ed32335fa6808e8f9316b9cae30ec
yaburi <nathan.au@hotmail.com>
2018-03-05 23:12:22 +1100

    initial Express scaffolding

---
+++ routes/api/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+
+// Remember to have this line last (generally good practice)
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'MusicList' });
-});
-
-const testJSON = [
-    {
-        name: 'John Smith',
-        username: 'CaptainCode'
-    },
-    {
-        name: 'Jane Doe',
-        username: 'JaneyCakes'
-    }
-];
-
-// If we receive an HTTP GET request to /sendjson
-router.get('/sendjson', (req, res, next) => {
-    res.json(testJSON); // respond with a json (testJSON) instead of res.render()
+  res.render('index', { title: 'MusicList Alpha' });
 });
 
 module.exports = router;
